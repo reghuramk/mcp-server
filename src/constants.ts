8f4ce7bcfcb8b1ebb12295c5a2c41ed60349643f
reghuramk <reghuramk04@gmail.com>
2025-04-07 15:32:27 +0100

    Created a weather MCP server

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+const USER_AGENT = "weather-app/1.0";
+const NWS_API_BASE = "https://api.weather.gov";
+
+export { USER_AGENT, NWS_API_BASE };
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import process from "node:process"
+
+// Create server instance
+export const server = new McpServer({
+  name: "weather",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
+
+
